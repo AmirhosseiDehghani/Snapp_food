e3b1e5f8b750aa40da0bded0012279f8150f38b0
MonsterDeh <dehghani.sad.1@gmail.com>
2022-06-24 01:31:16 +0430

    add gate admin & seller & buyer in aurh provide

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +27,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('Admin', function (User $user) {
+            return $user->role == Role::ADMIN; 
+        });
+        Gate::define('Seller', function (User $user) {
+            return $user->role == Role::SELLER; 
+        });
+        Gate::define('Buyer', function (User $user) {
+            return $user->role == Role::BUYER; 
+        });
     }
 }
